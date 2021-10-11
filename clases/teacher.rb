1addda590c37588de4b3b79f26dad25563ed4326
miguelesco <miuelangel1999@gmail.com>
2021-10-11 15:02:32 -0400

    add clases and initial config

---
+++ clases/teacher.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+
+class Teacher < Person
+  def initialize(specialization, age, name = 'Unkown')
+    super(age, name)
+    @specialization  = specialization 
+  end
+
+  def can_use_services?
+    return true
+  end
+end
+
+teacher = Teacher.new('Ruby', 30, 'Juan')
+teacher.can_use_services?
