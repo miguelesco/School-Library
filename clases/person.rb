928ff6fe1026ccb6b14955457a4871054ca0f401
miguelesco <miuelangelescorche@gmail.com>
2021-10-13 16:40:10 -0400

    student and classroom

--- clases/person.rb
+++ clases/person.rb
@@ -1,10 +1,10 @@
 require_relative 'corrector'
 
 class Person
-  attr_reader :id
   attr_accessor :name, :age
+  attr_reader :id, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- clases/student.rb
+++ clases/student.rb
@@ -1,15 +1,19 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name: 'Unkown', parent_permission: true)
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯\(ツ)/¯'
+    "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
-
-student = Student.new(18, 'Juan', 'A')
-puts student.play_hooky
